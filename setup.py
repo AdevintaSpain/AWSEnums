c989dcf5192c08c3a4a49069d873dcb8bdce361f
Ferran Grau Horta <ferran.grau@scmspain.com>
2017-11-06 11:33:55 +0100

    add structurefor the module

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name='AWSEnums',
+  packages=['AWSEnums'],
+  version='0.1',
+  description='Python module that contails a list of enums usefull for your aplications.',
+  author='Schibsted Spain',
+  author_email='ferran.grau@scmspain.com',
+  url='https://github.com/SchibstedSpain/AWSEnums',
+  download_url='https://github.com/SchibstedSpain/AWSEnums/archive/0.1.tar.gz',
+  keywords=['AWS', 'enums'],
+  classifiers=[]
+)
