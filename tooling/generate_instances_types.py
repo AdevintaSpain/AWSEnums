a0145c1e2c890738b843e115aac532717bdc6cc1
Ferran Grau Horta <ferran.grau@gmail.com>
2018-02-27 16:07:17 +0100

    change instance type generation to add all instances (#11)

--- tooling/generate_instances_types.py
+++ tooling/generate_instances_types.py
@@ -1,6 +1,8 @@
 import boto3
 import argparse
 
+from typing import List
+
 parser = argparse.ArgumentParser(description='Generate a aws instance type enum.')
 
 parser.add_argument('-r', '--region',
@@ -15,7 +17,22 @@ params = vars(parser.parse_args())
 session = boto3.Session(region_name=params["region"], profile_name=params["profile"])
 pricing = session.client('pricing')
 
-response = pricing.get_attribute_values(ServiceCode='AmazonEC2', AttributeName='instanceType')
+
+def get_instances(next_token: str=None) -> List[str]:
+    if next_token:
+        response = pricing.get_attribute_values(ServiceCode='AmazonEC2',
+                                                AttributeName='instanceType',
+                                                NextToken=next_token)
+    else:
+        response = pricing.get_attribute_values(ServiceCode='AmazonEC2', AttributeName='instanceType')
+    instances: List[str] = [instance['Value'] for instance in response['AttributeValues'] if "." in instance['Value']]
+    if "NextToken" in response.keys():
+        instances.extend(get_instances(next_token=response["NextToken"]))
+
+    return instances
+
+
+instance_types: List[str] = get_instances()
 
 path = 'type.py'
 python_file = open(path, 'w')
@@ -23,6 +40,5 @@ python_file.write("from enum import Enum\n")
 python_file.write("\n")
 python_file.write("\n")
 python_file.write("class Type(Enum):\n")
-for instance in response['AttributeValues']:
-    if "." in instance['Value']:
-        python_file.write("    {} = \"{}\"\n".format(instance['Value'].upper().replace(".", "_"), instance['Value']))
+for instance_type in instance_types:
+    python_file.write("    {} = \"{}\"\n".format(instance_type.upper().replace(".", "_"), instance_type))
